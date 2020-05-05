52008e72c91d15892508d69318db1d6121004090
Stefanos Malliaros <stefanos.malliaros@fetch.ai>
2020-05-05 12:30:39 +0100

    debugging

--- docker/ibc_txs.sh
+++ docker/ibc_txs.sh
@@ -1,50 +1,45 @@
 # #!/usr/bin/env bash
-# apt update && apt install -y wget
+rly config init
 
-# wget https://gist.githubusercontent.com/harsh-98/befc5e2076807aab102710b06db20567/raw/19b09779ee892d942c7b8a131bec930777d70d8b/chains.json
-
-# rly config init
-
-# echo '{"key":"faucetkey","chain-id":"fetchBeacon","rpc-addr":"http://fetch-goz.fetch.ai:26657","account-prefix":"cosmos","gas":200000,"gas-prices":"0.025ufet","default-denom":"ufet","trusting-period":"330h"}' > root/chain0.json
-# echo '{"key":"faucet","chain-id":"stackmybits","rpc-addr":"http://dockme.nl:26657","account-prefix":"cosmos","gas":200000,"gas-prices":"0.025bits","default-denom":"bits","trusting-period":"330h"}' > /root/chain1.json
+echo '{"key":"faucet","chain-id":"fetchBeacon","rpc-addr":"http://fetch-goz.fetch.ai:26657","account-prefix":"cosmos","gas":200000,"gas-prices":"0.025sharedtoken","default-denom":"sharedtoken","trusting-period":"330h"}' > root/chain0.json
+echo '{"key": "faucet","chain-id": "gameofzoneshub-1a","rpc-addr": "http://35.233.155.199:26657","account-prefix": "cosmos","gas": 200000,"gas-prices": "0.025doubloons","default-denom": "doubloons","trusting-period": "90m"}' > /root/chain1.json
 
 
-# rly chains add -f /root/chain0.json
-# rly chains add -f /root/chain1.json
-# echo "Chain configs added"
-# sleep 5
+rly chains add -f /root/chain0.json
+rly chains add -f /root/chain1.json
+echo "Chain configs added"
+sleep 5
 
-# rly lite init fetchBeacon -f
-# rly lite init stackmybits -f
+rly lite init fetchBeacon -f
+rly lite init gameofzoneshub-1a -f
+echo "Lite clients initiated"
+sleep 5
 
-# echo "Lite clients initiated"
-# sleep 5
 
+rly keys restore fetchBeacon accname "deer culture observe tent bitter depend moment train wasp approve erase grit seek cake bag truck program sea hub spin avoid crack rib skull"
+rly keys restore gameofzoneshub-1a accname "deer culture observe tent bitter depend moment train wasp approve erase grit seek cake bag truck program sea hub spin avoid crack rib skull"
+echo "Account created for both chains"
+sleep 5
 
-# rly keys restore fetchBeacon accname "deer culture observe tent bitter depend moment train wasp approve erase grit seek cake bag truck program sea hub spin avoid crack rib skull"
-# rly keys restore stackmybits accname "deer culture observe tent bitter depend moment train wasp approve erase grit seek cake bag truck program sea hub spin avoid crack rib skull"
-# echo "Account created for both chains"
-# sleep 5
+rly ch edit fetchBeacon key accname
+rly ch edit gameofzoneshub-1a key accname
+echo "Selected accname account as default for both chains"
+sleep 5
 
-# rly ch edit fetchBeacon key accname
-# rly ch edit stackmybits key accname
-# echo "Selected accname account as default for both chains"
-# sleep 5
-
-# rly tst request fetchBeacon accname
-# rly tst request stackmybits accname
+rly tst request fetchBeacon accname
+# rly tst request gameofzoneshub-1a accname
 # echo "Requested funds from both chains.sh"
-# sleep 5
+sleep 5
 
-# rly pth gen fetchBeacon transfer stackmybits transfer demopath
-# rly tx full-path demopath
-# echo "TX link verified"
+rly pth gen fetchBeacon transfer gameofzoneshub-1a transfer demopath
+rly tx full-path demopath
+echo "TX link verified"
 
 while true
 do
-# 	echo "Performing migration from fetchBeacon to stackmybits"
-# 	rly tx transfer fetchBeacon stackmybits "5ufet" true $(rly ch addr stackmybits)
-# 	rly q bal fetchBeacon
-# 	rly q bal stackmybits
+	echo "Performing migration from fetchBeacon to stackmybits"
+	rly tx transfer fetchBeacon gameofzoneshub-1a "5sharedtoken" true $(rly ch addr gameofzoneshub-1a)
+	rly q bal fetchBeacon
+	rly q bal stackmybits
     sleep 1800
 done 
