b4e6e3fb6faeadccc717408966000f7731405a0a
Stefanos Malliaros <stefanos.malliaros@fetch.ai>
2020-05-18 09:50:23 +0100

    Modified stuff

--- docker/ibc_txs.sh
+++ docker/ibc_txs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 rly config init
 
-echo '{"key":"faucet","chain-id":"fetchBeacon-1b","rpc-addr":"http://fetch-goz.fetch.ai:26657","account-prefix":"cosmos","gas":200000,"gas-prices":"0.025sharedtoken","default-denom":"sharedtoken","trusting-period":"11m"}' > root/chain0.json
-echo '{"key": "faucet","chain-id": "gameofzoneshub-1b","rpc-addr": "http://goz-node:26657","account-prefix": "cosmos","gas": 200000,"gas-prices": "0.025doubloons","default-denom": "doubloons","trusting-period": "11m"}' > /root/chain1.json
+echo '{"key":"faucet","chain-id":"fetchBeacon-1b","rpc-addr":"http://fetch-goz.fetch.ai:26657","account-prefix":"cosmos","gas":200000,"gas-prices":"0.025ufet","default-denom":"ufet","trusting-period":"9m"}' > root/chain0.json
+echo '{"key": "faucet","chain-id": "gameofzoneshub-1b","rpc-addr": "http://35.190.35.11:80","account-prefix": "cosmos","gas": 200000,"gas-prices": "0.025doubloons","default-denom": "doubloons","trusting-period": "9m"}' > /root/chain1.json
 
 
 rly chains add -f /root/chain0.json
@@ -27,27 +27,27 @@ echo "Selected accname account as default for both chains"
 sleep 5
 
 rly tst request fetchBeacon-1b accname
-rly tst request gameofzoneshub-1b accname
-echo "Requested funds from both chains.sh"
-sleep 5
+# rly tst request gameofzoneshub-1b accname
+# echo "Requested funds from both chains.sh"
+# sleep 5
 
 rly pth gen fetchBeacon-1b transfer gameofzoneshub-1b transfer demopath -f
 
-# s=fetchBeacon-1b && \
-# d=gameofzoneshub-1b && \
-p=demopath && \
-rly transact clients $p --debug &> out1.log ; wait && \
-rly transact connection $p --debug &> out2.log ; wait && \
-rly transact channel $p --debug &> out3.log ; wait && \
-rly pth show $p
-rly tx full-path demopath
-echo "TX link verified"
+
 
 while true
 do
+	
+	rly transact clients demopath
+	rly transact connection demopath
+	rly transact channel demopath
+	rly pth show demopath
+	echo "TX link verified"
+	sleep 5
+	echo "Doing IBC TX"
+	rly tx transfer fetchBeacon-1b gameofzoneshub-1b "5ufet" true $(rly ch addr gameofzoneshub-1b)
+	rly q bal fetchBeacon-1b
+	rly q bal gameofzoneshub-1b
 	echo "Sleeping"
-	# rly tx transfer fetchBeacon-1b gameofzoneshub-1b "5ufet" true $(rly ch addr gameofzoneshub-1b)
-	# rly q bal fetchBeacon-1b
-	# rly q bal gameofzoneshub-1b
-    sleep 3600
+    sleep 640
 done
